bbd240daafae5c528504baa8a4529be41127760e
ageha-chou <agehachou2602@gmail.com>
2021-10-12 22:41:15 +0700

    added news creation form (not use :()

--- lib/modules/events/screens/create_event_screen.dart
+++ lib/modules/events/screens/create_event_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,6 +5,7 @@ import 'package:html_editor_enhanced/html_editor.dart';
 import 'package:uni_alumni/modules/events/event_controller.dart';
 import 'package:uni_alumni/shared/constants/colors.dart';
 import 'package:uni_alumni/shared/widgets/sub_screen_app_bar.dart';
+import 'package:uni_alumni/shared/widgets/upload_image_holder.dart';
 
 class CreateEventScreen extends StatefulWidget {
   @override
@@ -14,7 +13,17 @@ class CreateEventScreen extends StatefulWidget {
 }
 
 class _CreateEventScreenState extends State<CreateEventScreen> {
+  final GlobalKey<UploadImageHolderState> _imageHolderKey = GlobalKey();
+  final GlobalKey<FormState> _formKey = GlobalKey();
   final eventController = Get.find<EventController>();
+  bool _isEventNameValid = true;
+  bool _isValidContent = true;
+  String? _contentErrorMsg;
+  bool _isValidLocation = true;
+  bool _isValidRegistrationStart = true;
+  bool _isValidRegistrationEnd = true;
+  bool _isValidEventStart = true;
+  bool _isValidEventEnd = true;
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +42,18 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
             ),
             child: GestureDetector(
               onTap: () async {
-                print(await eventController.htmlController.getText());
+                _formKey.currentState!.validate();
+
+                //image validation
+                _imageHolderKey.currentState!.validate();
+
+                //content validation
+                final _content =
+                    await eventController.contentController.getText();
+                _contentErrorMsg = _contentValidation(_content);
+                if (_contentErrorMsg != null) {
+                  setState(() {});
+                }
               },
               child: Icon(
                 Icons.save,
@@ -46,63 +66,87 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
       ),
       backgroundColor: ColorConstants.lightScaffoldBackgroundColor,
       body: Form(
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 15),
-          child: SingleChildScrollView(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 15.0),
             child: Column(
               children: [
                 _buildInputTextFormField(
                   label: 'Event Name*',
                   hintText: 'Name of the event',
+                  controller: eventController.eventNameController,
+                  validator: _eventNameValidation,
+                  isValid: _isEventNameValid,
                 ),
-                const SizedBox(height: 15),
-                Obx(() {
-                  if (eventController.isContainedImage()) {
-                    return _buildImageDisplay(
-                        label: 'Banner*',
-                        onTapHandler: eventController.showImageDialog);
-                  }
-
-                  return _buildUploadImage(
-                    context,
-                    label: 'Banner*',
-                    content: 'Upload Image',
-                    onTapHandler: eventController.showImageDialog,
-                  );
-                }),
-                const SizedBox(height: 15),
+                const SizedBox(height: 20),
+                UploadImageHolder(
+                  key: _imageHolderKey,
+                  label: 'Banner*',
+                  content: 'Upload Image',
+                ),
+                // Obx(() {
+                //   if (eventController.isContainedImage()) {
+                //     return _buildImageDisplay(
+                //         label: 'Banner*',
+                //         onTapHandler: eventController.showImageDialog);
+                //   }
+                //
+                //   return _buildUploadImage(
+                //     context,
+                //     label: 'Banner*',
+                //     content: 'Upload Image',
+                //     onTapHandler: eventController.showImageDialog,
+                //   );
+                // }),
+                const SizedBox(height: 20),
                 _buildTextEditor(
                   label: 'Description*',
-                  controller: eventController.htmlController,
+                  controller: eventController.contentController,
                 ),
-                const SizedBox(height: 15),
+                const SizedBox(height: 20),
                 _buildInputTextFormField(
                   label: 'Location*',
                   hintText: 'Location of the event',
+                  controller: eventController.locationController,
+                  validator: _locationValidation,
+                  isValid: _isValidLocation,
                 ),
-                const SizedBox(height: 15),
+                const SizedBox(height: 20),
                 _buildDateTextFormFieldsInRow(
                   firstLabel: 'Registration start*',
-                  secondLabel: 'Registration end*',
-                  hintText: 'dd/MM/yyyy',
                   firstOnTapHandler: () {
                     eventController.showDatePicker();
                   },
+                  firstController: eventController.registrationStartController,
+                  firstValidator: _registrationStartValidation,
+                  isFirstValid: _isValidRegistrationStart,
+                  secondLabel: 'Registration end*',
                   secondOnTapHandler: () {
                     eventController.showDatePicker();
                   },
+                  secondController: eventController.registrationStartController,
+                  secondValidator: _registrationEndValidation,
+                  isSecondValid: _isValidRegistrationEnd,
+                  hintText: 'dd/MM/yyyy',
                 ),
-                const SizedBox(height: 15),
+                const SizedBox(height: 20),
                 _buildDateTextFormFieldsInRow(
                   firstLabel: 'Event start*',
-                  secondLabel: 'Event end*',
-                  hintText: 'dd/MM/yyyy',
                   firstOnTapHandler: () {
                     eventController.showDatePicker();
                   },
+                  firstController: eventController.eventStartController,
+                  firstValidator: _eventStartValidation,
+                  isFirstValid: _isValidEventStart,
+                  secondLabel: 'Event end*',
                   secondOnTapHandler: () {
                     eventController.showDatePicker();
                   },
+                  secondController: eventController.eventEndController,
+                  secondValidator: _eventEndValidation,
+                  isSecondValid: _isValidEventEnd,
+                  hintText: 'dd/MM/yyyy',
                 ),
               ],
             ),
@@ -112,29 +156,136 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
     );
   }
 
+  String? _eventNameValidation(String? value) {
+    String? error;
+    if (value!.isEmpty) {
+      error = 'Please enter event name';
+    }
+
+    setState(() {
+      _isEventNameValid = error == null;
+    });
+
+    return error;
+  }
+
+  String? _contentValidation(String content) {
+    String? error;
+    if (content.isEmpty) {
+      error = 'Please enter content';
+    }
+
+    setState(() {
+      _isValidContent = content.isNotEmpty;
+    });
+
+    return error;
+  }
+
+  String? _locationValidation(String? value) {
+    String? error;
+    if (value!.isEmpty) {
+      error = 'Please enter event location';
+    }
+
+    setState(() {
+      _isValidLocation = error == null;
+    });
+
+    return error;
+  }
+
+  String? _registrationStartValidation(String? value) {
+    String? error;
+    if (value!.isEmpty) {
+      error = 'Please enter registration start';
+    }
+
+    setState(() {
+      _isValidRegistrationStart = error == null;
+    });
+
+    return error;
+  }
+
+  String? _registrationEndValidation(String? value) {
+    String? error;
+    if (value!.isEmpty) {
+      error = 'Please enter registration end';
+    }
+
+    setState(() {
+      _isValidRegistrationEnd = error == null;
+    });
+
+    return error;
+  }
+
+  String? _eventStartValidation(String? value) {
+    String? error;
+    if (value!.isEmpty) {
+      error = 'Please enter event start date';
+    }
+
+    setState(() {
+      _isValidEventStart = error == null;
+    });
+
+    return error;
+  }
+
+  String? _eventEndValidation(String? value) {
+    String? error;
+    if (value!.isEmpty) {
+      error = 'Please enter event end date';
+    }
+
+    setState(() {
+      _isValidEventEnd = error == null;
+    });
+
+    return error;
+  }
+
   Widget _buildTextField(
       {required String label,
       required String hintText,
       TextEditingController? controller,
       VoidCallback? onTapHandler,
-      bool readOnly = false}) {
+      bool readOnly = false,
+      String? Function(String?)? validator,
+      bool isValid = true}) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(label),
+        Text(
+          label,
+          style: TextStyle(color: isValid ? Colors.black : Colors.red),
+        ),
         const SizedBox(height: 8.0),
         TextFormField(
+          validator: validator,
           readOnly: readOnly,
           onTap: onTapHandler,
           controller: controller,
           style: TextStyle(fontSize: 13),
           decoration: InputDecoration(
             hintText: hintText,
+            errorStyle: errorText,
+            errorMaxLines: 3,
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10.0),
             ),
             focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.red),
               borderRadius: BorderRadius.circular(10.0),
             ),
           ),
@@ -155,11 +306,16 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(label),
+          Text(
+            label,
+            style:
+                TextStyle(color: _isValidContent ? Colors.black : Colors.red),
+          ),
           const SizedBox(height: 8.0),
           Container(
             decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
+              border: Border.all(
+                  color: _isValidContent ? Colors.black : Colors.red),
               borderRadius: BorderRadius.circular(10.0),
               color: Colors.transparent,
             ),
@@ -185,6 +341,17 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
               ],
             ),
           ),
+          if (_contentErrorMsg != null)
+            Padding(
+              padding: EdgeInsets.only(
+                top: 8,
+                left: 10,
+              ),
+              child: Text(
+                _contentErrorMsg!,
+                style: errorText,
+              ),
+            ),
         ],
       ),
     );
@@ -193,7 +360,9 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
   Widget _buildInputTextFormField(
       {required String label,
       required String hintText,
-      TextEditingController? controller}) {
+      TextEditingController? controller,
+      String? Function(String?)? validator,
+      bool isValid = true}) {
     return Container(
       margin: EdgeInsets.symmetric(
         horizontal: 40,
@@ -202,6 +371,8 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
         label: label,
         hintText: hintText,
         controller: controller,
+        validator: validator,
+        isValid: isValid,
       ),
     );
   }
@@ -214,6 +385,10 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
     TextEditingController? secondController,
     VoidCallback? firstOnTapHandler,
     VoidCallback? secondOnTapHandler,
+    String? Function(String?)? firstValidator,
+    String? Function(String?)? secondValidator,
+    bool isFirstValid = true,
+    bool isSecondValid = true,
   }) {
     return Row(
       children: [
@@ -228,6 +403,8 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
               hintText: hintText,
               controller: firstController,
               onTapHandler: firstOnTapHandler,
+              validator: firstValidator,
+              isValid: isFirstValid,
               readOnly: true,
             ),
           ),
@@ -243,6 +420,8 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
               hintText: hintText,
               controller: secondController,
               onTapHandler: secondOnTapHandler,
+              validator: secondValidator,
+              isValid: isSecondValid,
               readOnly: true,
             ),
           ),
@@ -251,84 +430,89 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
     );
   }
 
-  Widget _buildUploadImage(BuildContext context,
-      {required String label,
-      required String content,
-      VoidCallback? onTapHandler}) {
-    return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: 40,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(label),
-          const SizedBox(height: 8.0),
-          InkWell(
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-            onTap: onTapHandler,
-            child: Container(
-              width: double.infinity,
-              height: 180,
-              decoration: BoxDecoration(
-                border: Border.all(color: Color(0xFFb5b0ac)),
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.camera_alt,
-                    size: 50,
-                    color: Color(0xFFb5b0ac),
-                  ),
-                  Text(
-                    content,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  final TextStyle errorText = TextStyle(
+    color: Colors.red[700],
+    fontSize: 12,
+  );
 
-  Widget _buildImageDisplay(
-      {required String label, VoidCallback? onTapHandler}) {
-    return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: 40,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(label),
-          const SizedBox(height: 8.0),
-          InkWell(
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-            onTap: onTapHandler,
-            child: Container(
-              width: double.infinity,
-              height: 180,
-              decoration: BoxDecoration(
-                border: Border.all(color: Color(0xFFb5b0ac)),
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                child: Image.file(
-                  File(eventController.banner.value.path),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  // Widget _buildUploadImage(BuildContext context,
+  //     {required String label,
+  //     required String content,
+  //     VoidCallback? onTapHandler}) {
+  //   return Container(
+  //     margin: EdgeInsets.symmetric(
+  //       horizontal: 40,
+  //     ),
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.start,
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: [
+  //         Text(label),
+  //         const SizedBox(height: 8.0),
+  //         InkWell(
+  //           borderRadius: BorderRadius.all(Radius.circular(20.0)),
+  //           onTap: onTapHandler,
+  //           child: Container(
+  //             width: double.infinity,
+  //             height: 180,
+  //             decoration: BoxDecoration(
+  //               border: Border.all(color: Color(0xFFb5b0ac)),
+  //               borderRadius: BorderRadius.all(Radius.circular(20.0)),
+  //             ),
+  //             child: Column(
+  //               mainAxisAlignment: MainAxisAlignment.center,
+  //               children: [
+  //                 Icon(
+  //                   Icons.camera_alt,
+  //                   size: 50,
+  //                   color: Color(0xFFb5b0ac),
+  //                 ),
+  //                 Text(
+  //                   content,
+  //                   style: Theme.of(context).textTheme.headline6,
+  //                 ),
+  //               ],
+  //             ),
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+  //
+  // Widget _buildImageDisplay(
+  //     {required String label, VoidCallback? onTapHandler}) {
+  //   return Container(
+  //     margin: EdgeInsets.symmetric(
+  //       horizontal: 40,
+  //     ),
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.start,
+  //       crossAxisAlignment: CrossAxisAlignment.start,
+  //       children: [
+  //         Text(label),
+  //         const SizedBox(height: 8.0),
+  //         InkWell(
+  //           borderRadius: BorderRadius.all(Radius.circular(20.0)),
+  //           onTap: onTapHandler,
+  //           child: Container(
+  //             width: double.infinity,
+  //             height: 180,
+  //             decoration: BoxDecoration(
+  //               border: Border.all(color: Color(0xFFb5b0ac)),
+  //               borderRadius: BorderRadius.all(Radius.circular(20.0)),
+  //             ),
+  //             child: ClipRRect(
+  //               borderRadius: BorderRadius.all(Radius.circular(20.0)),
+  //               child: Image.file(
+  //                 File(eventController.banner.value.path),
+  //                 fit: BoxFit.cover,
+  //               ),
+  //             ),
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
