3512dcb26b7d4fa3b8208dc331a4ff40ad99e7bc
DELL <anhnpqse140645@gmail.com>
2021-10-22 06:44:25 +0700

    adjust referral

--- lib/modules/home/tabs/referral_tab.dart
+++ lib/modules/home/tabs/referral_tab.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import 'package:get/get.dart';
 import 'package:uni_alumni/modules/home/main_controller.dart';
+import 'package:uni_alumni/modules/refferal/screens/referral_moreinformation.dart';
 import 'package:uni_alumni/modules/refferal/screens/referral_list.dart';
 import 'package:uni_alumni/modules/refferal/screens/referral_registration.dart';
+import 'package:uni_alumni/modules/refferal/widges/referral_information_list.dart';
 import 'package:uni_alumni/shared/constants/assets.dart';
 import 'package:uni_alumni/shared/constants/colors.dart';
 
@@ -14,166 +17,68 @@ class ReferralTab extends GetView<MainController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: ColorConstants.lightScaffoldBackgroundColor,
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.white,
-          title: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.only(right: 15.0),
-                child: Image.asset(
-                  AssetConstants.logo,
-                  width: 40,
-                ),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(right: 15.0),
+              child: Image.asset(
+                AssetConstants.logo,
+                width: 40,
               ),
-              Text(
-                'ALUMNI',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: ColorConstants.primaryAppColor,
-                  fontSize: 20,
-                  fontFamily: 'Poppins',
-                  letterSpacing: 3,
-                ),
+            ),
+            Text(
+              'REFERRAL',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: ColorConstants.primaryAppColor,
+                fontSize: 20,
+                fontFamily: 'Poppins',
+                letterSpacing: 3,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        body: PageView.builder(
-          controller: _referralController,
-          itemBuilder: (context, index) {
-            return Column(children: <Widget>[
-              Image.asset(
-                'assets/images/alumni_background.jpg',
-                width: 400,
-                height: 250,
-                fit: BoxFit.fitWidth,
-              ),
-              SizedBox(height: 4),
-              Expanded(
-                child: Container(
-                  width: 350,
-                  decoration: BoxDecoration(
-                    color: ColorConstants.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
-                  ),
-                  child: SafeArea(
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(height: 11),
-                        Text(
-                          'The Alumni Referring Students Service',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 13),
-                        Text(
-                          'Let\'s more student become a member of \n our university  ',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 15,
-                          ),
-                        ),
-                        //Create new referral
-                        SizedBox(height: 25),
-                        FlatButton(
-                          splashColor: ColorConstants.lightGray,
-                          onPressed: () {
-                            Get.to(ReferralRegistration());
+      ),
+      body: Container
+        (
+          height: double.infinity,
+          child: Stack(
+              children: [
+                ReferralInformation(),
+                Positioned(
+                  bottom: 16,
+                  right: 16,
+                  child: SpeedDial(
+                    icon: Icons.category,
+                    backgroundColor: ColorConstants.lightPrimaryAppColor,
+                    spacing: 15,
+                    children: [
+                      SpeedDialChild(
+                        child: Icon(Icons.info_outline),
+                        label: 'More Information',
+                        backgroundColor: ColorConstants.lightPrimaryAppColor,
+                        onTap: () {
+                          Get.to(() => MoreInformation());
                           },
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            side: BorderSide(
-                              color: Colors.blue.shade600,
-                            ),
-                          ),
-                          child: Container(
-                            width: 160,
-                            height: 50,
-                            alignment: Alignment.center,
-                            child: Text(
-                              'Let\'s referral',
-                              style: TextStyle(
-                                color: Colors.blue.shade600,
-                                fontSize: 19,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                        //Manage referrers
-                        SizedBox(height: 20),
-                        FlatButton(
-                          splashColor: ColorConstants.lightGray,
-                          onPressed: () {
-                            Get.to(ReferralList());
-                          },
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            side: BorderSide(
-                              color: Colors.blue.shade500,
-                            ),
-                          ),
-                          child: Container(
-                            width: 160,
-                            height: 50,
-                            alignment: Alignment.center,
-                            child: Text(
-                              'Manage referrers',
-                              style: TextStyle(
-                                color: Colors.blue.shade600,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                        //More Information
-                        SizedBox(height: 20),
-                        FlatButton(
-                          splashColor: ColorConstants.lightGray,
-                          onPressed: () {
-
-                          },
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                            side: BorderSide(
-                              color: Colors.blue.shade600,
-                            ),
-                          ),
-                          child: Container(
-                            width: 160,
-                            height: 50,
-                            alignment: Alignment.center,
-                            child: Text(
-                              'More Information',
-                              style: TextStyle(
-                                color: Colors.blue.shade600,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                      SpeedDialChild(
+                        child: Icon(Icons.add),
+                        label: 'Add new referral',
+                        backgroundColor: ColorConstants.lightPrimaryAppColor,
+                        onTap: () {
+                          Get.to(() => ReferralRegistration());
+                        },
+                      ),
+                    ],
                   ),
                 ),
-              ),
-            ]);
-          },
-          itemCount: 1,
-        ));
+              ])
+      ),
+    );
   }
 }
 
---
+++ lib/modules/refferal/screens/referral_detail.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class ReferrallDetail extends StatelessWidget {
+  const ReferrallDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Referral')
+        ),
+        body: Center(
+          child: Text('Referral Detail')
+        ),
+    );
+  }
+}
